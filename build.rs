dd949e99f82ed627cfc9842e88c7fd28de460ea4
Xavier Lange <xrlange@gmail.com>
2023-10-03 12:35:33 -0400

    include linux-y paths

--- build.rs
+++ build.rs
@@ -1,9 +1,19 @@
 const CPP_VERSION_FLAG: &'static str = "-std=c++20";
 
 fn main() -> miette::Result<()> {
-    let rdkit_path = std::path::PathBuf::from("/opt/homebrew/include/rdkit");
-    let homebrew_path = std::path::PathBuf::from("/opt/homebrew/include");
-    let mut builder = autocxx_build::Builder::new("src/main.rs", &[&rdkit_path, &homebrew_path])
+    let include_paths: Vec<_> = vec![
+        "/usr/local/include",
+        "/usr/local/include/rdkit",
+        "/usr/include",
+        "/usr/include/rdkit",
+        "/opt/homebrew/include/rdkit",
+        "/opt/homebrew/include",
+    ]
+    .into_iter()
+    .map(|x| std::path::PathBuf::from(x))
+    .collect();
+
+    let mut builder = autocxx_build::Builder::new("src/main.rs", &include_paths)
         .extra_clang_args(&[CPP_VERSION_FLAG])
         .build()
         .unwrap();
